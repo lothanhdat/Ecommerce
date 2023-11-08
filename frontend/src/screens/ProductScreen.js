d1f5fb9fe6b01bb8a82cbef39c007464e0c91c03
lothanhdat <lothanhdat95@gmail.com>
2023-11-09 00:27:46 +0700

    first commit

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+function ProductScreen({ match }) {
+  const { id } = useParams();
+  const product = products.find((p) => p._id == id);
+  return (
+    <div>
+      <Link to="/" className="btn btn-light my-3">
+        Go back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fluit
+            className="rounded"
+          />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush" className="rounded">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Price: ${product.price}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price: </Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status: </Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Button disabled={product.countInStock == 0}>
+                    Add to cart
+                  </Button>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
---
+++ readme.md
@@ -0,0 +1,17 @@
+npx create-react-app frontend
+cd frontend
+npm start
+
+xóa file không cần: app.css,app.text, setupTest.js
+cài đặt bootstrap.min.css
+
+npm install react-bootstrap bootstrap
+
+install navbar from: https://react-bootstrap.netlify.app/docs/components/navbar/
+install font from: https://cdnjs.com/libraries?q=font
+
+npm install react-router-dom react-router-bootstrap
+
+virtualenv myenv
+myenv\scripts\activate
+pip install django
